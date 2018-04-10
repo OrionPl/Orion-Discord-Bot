abf47be9d490b22c66a5165c88e28f8b2861447b
OrionPl <mikolaj.kierski@outlook.com>
2018-04-10 16:36:08 +0200

    Init commit

---
+++ Orion Bot/Orion Bot/Minecraft Bot.cs
@@ -0,0 +1,58 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Net;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Bot__McServer_
+{
+    class Minecraft_Bot
+    {
+        DiscordSocketClient _client;
+        CommandService _commands;
+        IServiceProvider _services;
+
+        public Minecraft_Bot()
+        {
+
+        }
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string botToken = "NDMyOTQyNDYyOTQwNDc5NDk4.Da1Hpg.7gv-Uop9zOr8bt8TnHT6B1yU0DA";
+
+            await RegisterCommandAsync();
+
+            await _client.LoginAsync(TokenType.Bot, botToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task RegisterCommandAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync();
+
+            await _commands.AddModuleAsync(Assembly.GetEntryAssembly());
+        }
+
+        private Task HandleCommandAsync(SocketMessage arg)
+        {
+            await
+        }           
+    }
+}
---
+++ Orion Bot/Orion Bot/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Bot__McServer_
+{
+    class Program
+    {
+        StreamWriter WriteCommands;
+        BackgroundWorker StartServerWorker = new BackgroundWorker();
+
+        delegate void SetTextCallBack(string text);
+
+        string MaxRam = "1024";
+        string MinRam = "1024";
+        string server = @"C:\Servers\MC Servers\1.12.2 Vanilla\server.jar";
+
+        static void Main(string[] args)
+        {
+            if (Console.ReadLine() == "start server")
+            {
+                StartServer();
+            }
+        }
+
+        void StartServer()
+        {
+                Process Minecraft = new Process();
+                Minecraft.StartInfo.FileName = "CMD.exe";
+                Minecraft.StartInfo.CreateNoWindow = false;
+                Minecraft.StartInfo.RedirectStandardInput = true;
+                Minecraft.StartInfo.RedirectStandardOutput = true;
+                Minecraft.StartInfo.RedirectStandardError = true;
+                Minecraft.StartInfo.UseShellExecute = false;
+                Minecraft.OutputDataReceived += Minecraft_OutputDataReceived;
+                Minecraft.ErrorDataReceived += Minecraft_ErrorDataReceived;
+                Minecraft.Start();
+                Minecraft.BeginOutputReadLine();
+                Minecraft.BeginErrorReadLine();
+                WriteCommands = Minecraft.StandardInput;
+                WriteCommands.WriteLineAsync(@"java -Xmx" + MaxRam + "M -Xms" + MinRam + "M -jar " + server + " " + "nogui");
+        }
+    }
+}
