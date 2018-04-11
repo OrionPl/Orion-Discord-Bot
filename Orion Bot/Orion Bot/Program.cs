380c46c17981c614ad8c8dc044925c820eeefa9f
Mikołaj Kierski <mikolaj.kierski@outlook.com>
2018-04-11 17:15:18 +0200

    Added a new form

--- Orion Bot/Orion Bot/Program.cs
+++ Orion Bot/Orion Bot/Program.cs
@@ -1,136 +1,22 @@
-﻿using Discord;
-using Discord.Commands;
-using Discord.Net;
-using Discord.WebSocket;
-using Microsoft.Extensions.DependencyInjection;
-using Orion_Bot;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
 
-namespace Discord_Bot__McServer_
+namespace Orion_Bot
 {
-    class Program
+    static class Program
     {
-        StreamWriter WriteCommands;
-        BackgroundWorker StartServerWorker = new BackgroundWorker();
-
-        delegate void SetTextCallBack(string text);
-
-        string MaxRam = "1024";
-        string MinRam = "1024";
-        string server = @"C:\Servers\MC Servers\1.12.2 Vanilla\server.jar";
-
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            Program program = new Program();
-
-            program.RunBotAsync();
-
-            program.StartServerWorker.DoWork += program.StartServer;
-
-
-            //if (Console.ReadLine() == "write")
-            //{
-            //    program.WriteInMC("lol");
-            //}
-
-            //if (Console.ReadLine() == "start minecraft server")
-            //{
-            //   program.StartServerWorker.RunWorkerAsync();
-            //}
-            if (Console.ReadLine() == "start ui")
-            {
-                Main mainUI = new Main();
-                mainUI.Show();
-            }
-            Console.ReadLine();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
-
-        public void StartServer(object sender, DoWorkEventArgs e)
-        {
-                Process Minecraft = new Process();
-                Minecraft.StartInfo.FileName = "CMD.exe";
-                Minecraft.StartInfo.CreateNoWindow = false;
-                Minecraft.StartInfo.RedirectStandardInput = true;
-                Minecraft.StartInfo.RedirectStandardOutput = true;
-                Minecraft.StartInfo.RedirectStandardError = true;
-                Minecraft.StartInfo.UseShellExecute = false;
-                Minecraft.OutputDataReceived += OutputDataReceived;
-                Minecraft.ErrorDataReceived += Minecraft_ErrorDataReceived;
-                Minecraft.Start();
-                Minecraft.BeginOutputReadLine();
-                Minecraft.BeginErrorReadLine();
-                WriteCommands = Minecraft.StandardInput;
-                WriteCommands.WriteLineAsync(@"java -Xmx" + MaxRam + "M -Xms" + MinRam + "M -jar " + server + " " + "nogui");
-        }
-
-        void Minecraft_ErrorDataReceived(object sender, DataReceivedEventArgs e)
-        {
-            Console.WriteLine("Error: " + e.Data);
-        }
-
-        void OutputDataReceived(object sender, DataReceivedEventArgs e)
-        {
-            WriteInMC(e.Data);
-        }
-
-
-
-        /////////////////////////////////////////////////////
-        DiscordSocketClient _client;
-        CommandService _commands;
-        IServiceProvider _services;
-
-        public ulong mcChannelID = 432947016298004490;
-        public ulong mcServerID = 432945835828051979;
-
-        public async Task RunBotAsync()
-        {
-            _client = new DiscordSocketClient();
-            _commands = new CommandService();
-            _services = new ServiceCollection()
-                .AddSingleton(_client)
-                .AddSingleton(_commands)
-                .BuildServiceProvider();
-
-            string botToken = "NDMyOTQyNDYyOTQwNDc5NDk4.Da1Hpg.7gv-Uop9zOr8bt8TnHT6B1yU0DA";
-
-            //await RegisterCommandAsync();
-
-            await _client.LoginAsync(TokenType.Bot, botToken);
-
-            await _client.StartAsync();
-
-            await Task.Delay(-1);
-        }
-
-        void WriteInMC(string text)
-        {
-            _client.GetGuild(mcServerID).GetTextChannel(mcChannelID).SendMessageAsync(text);
-        }
-
-        //public async Task RegisterCommandAsync()
-        //{
-        //    _client.MessageReceived += HandleCommandAsync;
-
-        //    await _commands.AddModuleAsync(Assembly.GetEntryAssembly());
-        //}
-
-        //private async Task HandleCommandAsync(SocketMessage arg)
-        //{
-        //    var message = arg as SocketMessage;
-
-        //    if (message is null || message.Author.IsBot)
-        //        return;
-        //}
-        /////////////////////////////////////////////////////
     }
 }
--- Orion Bot/Orion Bot/Properties/AssemblyInfo.cs
+++ Orion Bot/Orion Bot/Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
-// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
-// powiązane z zestawem.
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("Orion Bot")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +14,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
-// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
-// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
-[assembly: Guid("6d998757-4d8d-455e-a4cc-291c293041c9")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("28194b99-fc12-47f6-be4f-9dbcc53acd0d")]
 
-// Informacje o wersji zestawu zawierają następujące cztery wartości:
+// Version information for an assembly consists of the following four values:
 //
-//      Wersja główna
-//      Wersja pomocnicza
-//      Numer kompilacji
-//      Rewizja
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
 //
-// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
-// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
